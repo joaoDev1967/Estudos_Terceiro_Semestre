1e085c431f7af95b8afda584cd1edb0bdea24d6c
Johnny <joaopedrosdantas5@gmail.com>
2025-04-03 11:22:42 -0300

    Conatdor de vogais e consoantes adicionados ao código

--- retornoDeTamanhoDePalavra.c
+++ retornoDeTamanhoDePalavra.c
@@ -1,14 +1,30 @@
 #include<stdio.h>
 #include<string.h>
+// #include<stdbool.h>
 
 char palavra[100];
+char vogais[6] = {'a','e','i','o','u'};
 
 int contadorDeCaracteres(char palavra[100]){
     return strlen(palavra);
+}
+
+int contadorDeVogais(char palavra[100]){
+    int numVogais=0;
+    for(int i=0; i<strlen(palavra); i++){
+        for(int x=0; x<5;x++){
+            if(palavra[i]==vogais[x]){
+                numVogais++;
+            }
+        }
+    }
+    return numVogais;
 }
 
 void main(){
     printf("Escreva uma palavra: ");
     scanf("%s",palavra);
     printf("\n\n\nTamanho da palavra: %d",contadorDeCaracteres(palavra));
+    printf("\nNúmero de vogais:%d",contadorDeVogais(palavra));
+    printf("\nNúmero de consoantes:%d",strlen(palavra)-contadorDeVogais(palavra));
 }
